a9e1b7765b974967c2cc97603db688b7fc87f75f
Mohid <mohidtheshariff27@gmail.com>
2022-04-11 01:42:10 +0000

    commented files

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -17,18 +17,18 @@ download.file(
   mode="wb"
 )
 
-raw_pdf <- pdf_text("inputs/Pakistan National Health Survey.pdf") |>
+raw_pdf <- pdf_text("inputs/Pakistan National Health Survey.pdf") |> # read the pdf into a variable
   read_lines()
 
-raw_data <- raw_pdf[1492:1549] # the section of the pdf with our table (spans 2 pages)
+raw_data <- raw_pdf[1492:1549] # the section of the pdf with our table
 
-raw_data <- tibble(raw_data)
+raw_data <- tibble(raw_data) # create a table of the raw data
 
-write.csv(raw_data, "inputs/data/raw_data.csv", row.names=FALSE)
+write.csv(raw_data, "inputs/data/raw_data.csv", row.names=FALSE) # write the raw data into a cvs file
 
 #### What's next? ####
 
-raw_data <- raw_data[-c(1:5, 19, 24,30:34, 48, 53), ]
+raw_data <- raw_data[-c(1:5, 19, 24,30:34, 48, 53), ] # get rid of unnecessary lines in the raw data
 
 
 
@@ -38,7 +38,7 @@ colnames(raw_data) <- c("x")
 raw_data <- raw_data |>
   mutate(x=str_squish(x))
 
-# obtain the datapoints to fill a 26x12 matrix
+# obtain the datapoints to fill a 22x9 matrix
 mat <- matrix(rep(0, 198), nrow=22, byrow=TRUE)
 counter = 1
 
@@ -49,28 +49,27 @@ for (i in 1:22) {
   if (length(numbers) >= 10) {
     
     for (j in 1:length(numbers)) {
-      numbers[j] <- str_replace(numbers[j][1], "-", ".") # fix all - into decimal points .
-      numbers[j] <- str_replace(numbers[j][1], "_", ".") # fix all - into decimal points .
-      numbers[j] <- str_replace(numbers[j][1], " .", ".") # fix all ' '. into decimal points .
+      numbers[j] <- str_replace(numbers[j][1], "-", ".") # fix all "-", "_", " .", ",", "`" into decimal points
+      numbers[j] <- str_replace(numbers[j][1], "_", ".") 
+      numbers[j] <- str_replace(numbers[j][1], " .", ".") 
       numbers[j] <- str_replace(numbers[j][1], ",", ".")
       numbers[j] <- str_replace(numbers[j][1], "`", ".")
     }
     
-    # take in the numbers but start from the back and move forward, until we have 12
-    # this avoids the problem of states being in two indices at the front because of their spaces
-    numbers <- rev(numbers) # reverse the numbers (hit it from behind)
-    numbers <- numbers[1:9] # first 12 are never names/non-numeric (take what we want)
-    numbers <- rev(as.numeric(numbers)) # now we have the row values in correct order too (turn it back around)
+    # take in the numbers but start from the back and move forward, until we have 9
+    numbers <- rev(numbers) # reverse the numbers
+    numbers <- numbers[1:9] # first 9 are never names/non-numeric
+    numbers <- rev(as.numeric(numbers)) # now we have the row values in correct order
     
     mat[counter, ] <- numbers # add numbers as a row of mat
-    counter <- counter + 1
+    counter <- counter + 1 # increase counter by 1
   }
   # otherwise we reject this row because it was missing data or not a row we care about
 }
 
-# column_names <- c(1:23) # TODO: Put the column names here (omit state)
+# set up appropriate column names
 column_names <- c("No Education", "Primary", "Middle", "Secondary+", "Missing", "Total", "Population", "Median years", "Mean years")
-# state_names <- c(1:26) # TODO: Put the state names here
+# set up appropriate row names
 row_names <- c("5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65+", "Total Urban", "Major city", "Other urban", "Rural", "Punjab", "Sindh", "NWFP", "Balochistan", "Total")
 
 # after cleaning, construct the final data frame
@@ -78,16 +77,17 @@ final_df <- data.frame(mat)
 colnames(final_df) <- column_names
 final_df$`Background Characteristic` <- row_names
 
-# Make states the first column for visual nice
+# Make the first column background characteristic
 final_df <- final_df |>
   select(`Background Characteristic`, everything())
 
-# save it to a csv file so we only need to run this script once at the beginning of our journey
+# save it to a csv file
 write.csv(final_df, "inputs/data/males_data.csv", row.names=FALSE)
 
+# now get rid of all processed lines
 raw_data <- raw_data[-c(1:22), ]
 
-
+# create our matrix for the second table for "females"
 mat1 <- matrix(rep(0, 198), nrow=22, byrow=TRUE)
 counter = 1
 
@@ -98,48 +98,48 @@ for (i in 1:22) {
   if (length(numbers) >= 10) {
     
     for (j in 1:length(numbers)) {
-      numbers[j] <- str_replace(numbers[j][1], "-", ".") # fix all - into decimal points .
-      numbers[j] <- str_replace(numbers[j][1], "_", ".") # fix all - into decimal points .
-      numbers[j] <- str_replace(numbers[j][1], " .", ".") # fix all ' '. into decimal points .
+      numbers[j] <- str_replace(numbers[j][1], "-", ".") # fix all "-", "_", " .", ",", "`" into decimal points
+      numbers[j] <- str_replace(numbers[j][1], "_", ".") 
+      numbers[j] <- str_replace(numbers[j][1], " .", ".") 
       numbers[j] <- str_replace(numbers[j][1], ",", ".")
       numbers[j] <- str_replace(numbers[j][1], "`", ".")
     }
+    # fix all unorthodox cases into decimal points .
     if (i == 1){
       for (j in 1:length(numbers)) {
-        numbers[j] <- str_replace(numbers[j][1], "~", "40") # fix all - into decimal points .
+        numbers[j] <- str_replace(numbers[j][1], "~", "40")
     }
     }
     if (i == 13){
       for (j in 1:length(numbers)) {
-        numbers[j] <- str_replace(numbers[j][1], "~", "0.") # fix all - into decimal points .
+        numbers[j] <- str_replace(numbers[j][1], "~", "0.") 
       }
     }
     if (i == 18){
       for (j in 1:length(numbers)) {
-        numbers[j] <- str_replace(numbers[j][1], "A", ".1") # fix all - into decimal points .
+        numbers[j] <- str_replace(numbers[j][1], "A", ".1") 
       }
     }
-    # take in the numbers but start from the back and move forward, until we have 12
-    # this avoids the problem of states being in two indices at the front because of their spaces
-    numbers <- rev(numbers) # reverse the numbers (hit it from behind)
-    numbers <- numbers[1:9] # first 12 are never names/non-numeric (take what we want)
-    numbers <- rev(as.numeric(numbers)) # now we have the row values in correct order too (turn it back around)
+    # take in the numbers but start from the back and move forward, until we have 9
+    numbers <- rev(numbers) # reverse the numbers 
+    numbers <- numbers[1:9] # first 9 are never names/non-numeric
+    numbers <- rev(as.numeric(numbers)) # now we have the row values in correct order
     
-    mat1[counter, ] <- numbers # add numbers as a row of mat
-    counter <- counter + 1
+    mat1[counter, ] <- numbers # add numbers as a row of mat1
+    counter <- counter + 1 # increase counter by 1
   }
   # otherwise we reject this row because it was missing data or not a row we care about
 }
 
-# after cleaning, construct the final data frame
+# after cleaning, construct the final data frame for our second table
 final_df <- data.frame(mat1)
 colnames(final_df) <- column_names
 final_df$`Background Characteristic` <- row_names
 
-# Make states the first column for visual nice
+# Make first column background characteristic
 final_df <- final_df |>
   select(`Background Characteristic`, everything())
 
-# save it to a csv file so we only need to run this script once at the beginning of our journey
+# save it to a csv file
 write.csv(final_df, "inputs/data/females_data.csv", row.names=FALSE)
          
